a9bba94aaa032bc8f515827c3143d22584304fbd
Nicola Menonna <nicola.menonna@aulab.it>
2023-07-31 10:14:39 +0200

    init project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        {{-- My style CSS --}}
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+    <body class="antialiased">
+        <h1>Hack 85</h1>
+
+        <img src="/media/castel-del-monte-gd339414e8_1920.jpg" alt="">
+
+        <script src="/js/main.js"></script>
+    </body>
+</html>
